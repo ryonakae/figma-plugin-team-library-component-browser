71fafa6fc0bbdd7eb7db3a39a6624a66ff122c16
Ryo Nakae <ryo.n.1129@gmail.com>
2020-02-19 18:06:05 +0900

    save data wip

--- src/app/components/Setting.tsx
+++ src/app/components/Setting.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react'
 
 type MyProps = {}
-type MyState = {}
+type MyState = {
+  isFetching: boolean
+}
 
 export default class Setting extends React.Component<MyProps, MyState> {
   constructor(props) {
     super(props)
+    this.state = {
+      isFetching: false
+    }
+  }
+
+  async handleClick(): Promise<void> {
+    this.setState({ isFetching: true })
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: 'save'
+        }
+      },
+      '*'
+    )
   }
 
   componentDidMount(): void {
@@ -17,6 +34,23 @@ export default class Setting extends React.Component<MyProps, MyState> {
   }
 
   render(): JSX.Element {
-    return <div>Setting</div>
+    return (
+      <div>
+        <div>
+          <span>Save Team Library Data</span>
+        </div>
+        <div>
+          <p>
+            The quick brown fox jumps over the lazy dog. The quick brown fox
+            jumps over the lazy dog. The quick brown fox jumps over the lazy
+            dog. The quick brown fox jumps over the lazy dog. The quick brown
+            fox jumps over the lazy dog.
+          </p>
+        </div>
+        <div onClick={this.handleClick.bind(this)}>
+          Save or update this library data
+        </div>
+      </div>
+    )
   }
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,3 +1,50 @@
+export type MessageType = 'save'
+export type Component = {
+  name: string
+  id: string
+  key: string
+}
+export type Page = {
+  name: string
+  id: string
+  components: Component[]
+}
+export type Document = {
+  name: string
+  id: string
+  pages: Page[]
+}
+export type Library = Document[]
+
 figma.showUI(__html__)
 
-console.log(figma.root)
+async function saveComponentsData(): Promise<void> {
+  console.log('saveComponentsData')
+  const pages: Page[] = []
+
+  figma.root.children.forEach(page => {
+    const components: Component[] = []
+
+    page.children.forEach(scene => {
+      if (scene.type === 'COMPONENT') {
+        components.push({
+          name: scene.name,
+          id: scene.id,
+          key: scene.key
+        })
+      }
+    })
+
+    pages.push({
+      name: page.name,
+      id: page.id,
+      components
+    })
+  })
+
+  console.log(pages)
+}
+
+figma.ui.onmessage = async (msg): Promise<void> => {
+  if (msg.type === 'save') await saveComponentsData()
+}
